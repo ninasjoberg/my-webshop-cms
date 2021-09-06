b338aa25aceb8349539aa07e3b4ad81f98b6c14e
ninasjoberg <nina.sjoberg@gmail.com>
2021-09-06 18:58:40 +0200

    add size field and google title field, remove unused yarn.lock

--- schemas/product.js
+++ schemas/product.js
@@ -1,82 +1,99 @@
 export default {
-  name: 'product',
-  title: 'Product',
-  type: 'document',
+  name: "product",
+  title: "Product",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string'
+      name: "title",
+      title: "Title",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
-        maxLength: 96
-      }
+        source: "title",
+        maxLength: 96,
+      },
     },
     {
-      name: 'body',
-      title: 'Body',
-      type: 'localeBlockContent'
+      name: "titleForGoogleSearch",
+      title: "TitleForGoogleSearch",
+      type: "string",
     },
     {
-      name: 'images',
-      title: 'Images',
-      type: 'array',
+      name: "body",
+      title: "Body",
+      type: "localeBlockContent",
+    },
+    {
+      name: "images",
+      title: "Images",
+      type: "array",
       of: [
         {
-          type: 'image',
+          type: "image",
           options: {
-            hotspot: true
+            hotspot: true,
           },
           fields: [
             {
-              name: 'alt',
-              type: 'string',
-              title: 'Alt',
+              name: "alt",
+              type: "string",
+              title: "Alt",
               options: {
-                isHighlighted: true // <-- make this field easily accessible
-              }
-            }
-          ]
-        }
-      ]
+                isHighlighted: true, // <-- make this field easily accessible
+              },
+            },
+          ],
+        },
+      ],
     },
     {
-      title: 'Price SEK',
-      name: 'price',
-      type: 'number'
+      title: "Price SEK",
+      name: "price",
+      type: "number",
     },
     {
-      quantity: 'Unlimited',
-      name: 'quantity',
-      type: 'string'
+      quantity: "Unlimited",
+      name: "quantity",
+      type: "string",
     },
     {
-      title: 'Variants',
-      name: 'variants',
-      type: 'array',
+      title: "Variants",
+      name: "variants",
+      type: "array",
       of: [
         {
-          title: 'Variant',
-          name: 'variant',
-          type: 'productVariant'
-        }
-      ]
+          title: "Variant",
+          name: "variant",
+          type: "productVariant",
+        },
+      ],
     },
     {
-      title: 'Categories',
-      name: 'categories',
-      type: 'array',
+      title: "Size",
+      name: "size",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: { type: 'category' }
-        }
-      ]
+          title: "Variant",
+          name: "variant",
+          type: "productVariant",
+        },
+      ],
+    },
+    {
+      title: "Categories",
+      name: "categories",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: { type: "category" },
+        },
+      ],
     },
     {
       name: "order",
@@ -88,9 +105,9 @@ export default {
 
   preview: {
     select: {
-      title: 'title',
-      manufactor: 'manufactor.title',
-      media: 'defaultProductVariant.images[0]'
-    }
-  }
-}
+      title: "title",
+      manufactor: "manufactor.title",
+      media: "defaultProductVariant.images[0]",
+    },
+  },
+};
