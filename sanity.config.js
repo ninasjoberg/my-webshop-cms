d542f48e174d56b81408a2a403e095db9bf9fef7
Nina Sjöberg <nina.sjoberg@gmail.com>
2023-10-30 18:48:11 +0100

    finish update

--- sanity.config.js
+++ sanity.config.js
@@ -3,19 +3,11 @@ import { defineConfig } from "sanity";
 import { deskTool } from 'sanity/desk'
 import schemas from './schemas/schema'
 
-
 export default defineConfig({
   title: "web-shop-cms",
   projectId: "37lcj78e",
   dataset: "products",
-  plugins: [
-    deskTool({
-      structure: (S) =>
-      S.list()
-        .title('Base')
-        .items([...S.documentTypeListItems().reverse()]),
-    })
-  ],
+  plugins: [deskTool()],
   schema: {
     types: schemas,
   },
--- schemas/page.js
+++ schemas/page.js
@@ -32,9 +32,6 @@ export default {
               name: 'alt',
               type: 'string',
               title: 'Alt',
-              options: {
-                isHighlighted: true // <-- make this field easily accessible
-              }
             }
           ]
         }
